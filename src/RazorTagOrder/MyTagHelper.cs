6b835c7c0d05ca85a93c1094ac970888f47653eb
Przemysław Onak <ponak@asco.com.pl>
2016-06-16 13:23:02 +0200

    Initial commit

---
+++ src/RazorTagOrder/MyTagHelper.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace RazorTagOrder
+{
+    [HtmlTargetElement(Attributes = "href")]
+    public class MyTagHelper : TagHelper
+    {
+        /// <summary>
+        ///     Src
+        /// </summary>
+        [HtmlAttributeName("href")]
+        public string Href { get; set; }
+        
+        //public override int Order => int.MaxValue;
+
+        public override int Order => int.MinValue;
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.SetAttribute("href", "http://localhost:20299" + Href);
+        }
+    }
+}
